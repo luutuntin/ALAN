0eb10abd35ae1a6c4be6d539b1afcb2e12f0bf7f
AmaMidzu <anna@astori.fr>
2018-05-08 21:42:35 -0400

    update folia2spacy

--- folia2spacy.py
+++ folia2spacy.py
@@ -3,6 +3,11 @@
 from pynlpl.formats import folia
 import sys
 import spacy
+import getopt
+
+def usage():
+    print("This script converts a folia xml file into a spacy Doc object")
+    print("Usage: python3 folia2spacy.py folia-file")
 
 def build_spacy_doc(folia_file):
     try:
@@ -14,6 +19,23 @@ def build_spacy_doc(folia_file):
         print("Error occurred while processing " + folia_file)
 
 def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "-h", ["help"])
+    except getopt.GetoptError as err:
+        print(str(err),file=sys.stderr)
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o == '-h' or o == '--help':
+            usage()
+            sys.exit(0)
+        else:
+            raise Exception("No such option: " + o)
+
+    if len(args) < 1:
+        usage()
+        sys.exit(2)
     doc = build_spacy_doc(sys.argv[1])
 
 if __name__ == '__main__':
